3d5874686516c9df7cdbbbc4121aa692a53ce266
KhaiTran <TranKhai203@gmail.com>
2025-08-31 18:31:52 +0700

    Toi moi them func total

--- text.cpp
+++ text.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int total(int& a, int& b){
+    return a + b;
+}
+
 int main(){
     int a = 10;
     int b = 20;
-    cout << a + b;
+    cout << a + b << endl;
+    cout << total(a,b);
     return 0;
 }
